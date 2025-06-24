0599064b4783e813a5dfa114036335b8ab602ef2
Life Giver <lifefotsing@gmail.com>
2025-06-24 19:35:09 +0100

    Addition of default fxn

--- server/api/auth.js
+++ server/api/auth.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import authRoutes from '../routes/index.js'; // adjust path if needed
+import authRoutes from '../routes/index.js';
 
 const app = express();
 app.use(express.json());
