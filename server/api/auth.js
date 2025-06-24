2b8d1df64f099a4e5e14e7e1182e658e3c893ebb
Life Giver <lifefotsing@gmail.com>
2025-06-24 13:14:11 +0100

    Formating of code

--- server/api/auth.js
+++ server/api/auth.js
@@ -5,6 +5,6 @@ const app = express();
 app.use(express.json());
 app.use(authRoutes);
 
-export default (req, res) => {
-    app(req, res); // Vercel wraps Express like this
-};
+export default function handler(req, res) {
+    app(req, res);
+}
