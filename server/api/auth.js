a2134c89b13dbebd510b3ff0e6666ad347e06c93
Life Giver <lifefotsing@gmail.com>
2025-06-24 19:36:35 +0100

    Addition of default fxn

--- server/api/auth.js
+++ server/api/auth.js
@@ -5,6 +5,10 @@ const app = express();
 app.use(express.json());
 app.use(authRoutes);
 
+app.get('/ping', (req, res) => {
+    res.status(200).json({ message: 'Serverless is working!' });
+});
+
 export default function handler(req, res) {
     app(req, res);
 }
