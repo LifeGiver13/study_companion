ed016431b06fc9e16d61105540e2dd7361a8d016
Life Giver <lifefotsing@gmail.com>
2025-06-24 14:00:16 +0100

    Formating for vercel deployment

--- server/app.js
+++ server/app.js
@@ -1,13 +1,24 @@
-import express from 'express'
-const app = express()
-const port = 3000
+// import express from 'express'
+// const app = express()
+// const port = 3000
 
-import authRoute from './routes/index.js'
+// import authRoute from './routes/index.js'
 
-app.use(express.json())
-app.use(authRoute)
+// app.use(express.json())
+// app.use(authRoute)
 
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+// })
+
+import express from 'express';
+import authRoutes from '../routes/index.js';
+
+const app = express();
+app.use(express.json());
+app.use(authRoutes);
+
+export default function handler(req, res) {
+    app(req, res);
+}
