a5386b373bbc6c21f274b4e4b2d20315dc5e81f3
Life Giver <lifefotsing@gmail.com>
2025-06-26 07:54:30 +0100

    Adaptation of code to match vercel

--- server/app.js
+++ server/app.js
@@ -5,6 +5,6 @@ const app = express()
 
 
 app.use(express.json())
-app.use(authRoute)
+app.use('/api', authRoute);
 
 export default app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,16 @@
-import express from 'express';
-import { register } from '../../src/handlers/auth/registration.js';
-import { passwordReset } from '../../src/handlers/auth/passwordReset.js';
-import { login } from '../../src/handlers/auth/login.js';
+import express from 'express'
+import { register } from '../../src/handlers/auth/registration.js'
+import { login } from '../../src/handlers/auth/login.js'
+import { passwordReset } from '../../src/handlers/auth/passwordReset.js'
 
-const router = express.Router();
+const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('Hello from Vercel!')
-});
+router.get('/hello', (req, res) => {
+    res.send('Hello from API!')
+})
 
-router.post('/auth/register', register);
-router.post('/auth/reset-password', passwordReset);
+router.post('/auth/register', register)
 router.post('/auth/login', login)
-export default router;
+router.post('/auth/reset-password', passwordReset)
+
+export default router
