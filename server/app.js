f7cf88d7b9a7898969ba414f5a31724f7ed94c41
Life Giver <lifefotsing@gmail.com>
2025-06-27 20:13:37 +0100

    Addition of conditional for deployment platform

--- server/app.js
+++ server/app.js
@@ -7,4 +7,10 @@ const app = express()
 app.use(express.json())
 app.use(authRoute)
 
+if (process.env.RENDER === 'true' || process.env.NODE_ENV !== 'production') {
+    app.listen(port, () => {
+        console.log(`Server is running in development mode on port ${port}`);
+    })
+}
+
 export default app;
