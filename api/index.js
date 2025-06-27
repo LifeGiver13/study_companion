13771537e592147dabd33f62f212635e99a3b600
Life Giver <lifefotsing@gmail.com>
2025-06-27 15:36:30 +0100

    Addition of loop

--- api/index.js
+++ api/index.js
@@ -1,4 +1,3 @@
 import app from '../server/app.js';
-import serverless from 'serverless-http';
 
-export default serverless(app);
+export default app;
--- server/app.js
+++ server/app.js
@@ -6,4 +6,9 @@ const app = express();
 app.use(express.json());
 app.use('/api', authRoutes);
 
+if (process.env.RENDER === 'true' || process.env.NODE_ENV !== 'production') {
+    app.listen(port, () => {
+        console.log(`Server is running in development mode on port ${port}`);
+    })
+}
 export default app;
